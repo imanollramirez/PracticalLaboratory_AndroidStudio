71be6768a73ab5ef877a8bae3d2d3fb962616f2e
Imanol <20230065@ricaldone.edu.sv>
2024-05-03 23:33:18 -0600

    Proyecto -> 95% (Profesor, los constraints siempre se me desordenan, llevo un buen rato intentando ponerlos

--- app/src/main/java/imanol/ramirez/emulator_20230065_imanol/Activity_Login.kt
+++ app/src/main/java/imanol/ramirez/emulator_20230065_imanol/Activity_Login.kt
@@ -1,6 +1,8 @@
 package imanol.ramirez.emulator_20230065_imanol
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class Activity_Login : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnLogin = findViewById<Button>(R.id.btnIngresar)
+
+        btnLogin.setOnClickListener{
+            val Emulator = Intent(this,MainActivity::class.java)
+            startActivity(Emulator);
         }
     }
 }
